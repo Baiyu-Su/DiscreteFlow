c60be953e88be903ec5813afaf0bb832f6b34794
Baiyu Su <baiyusu@c611-061.vista.tacc.utexas.edu>
2025-01-12 12:15:52 -0600

    Bash.

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH -J llama
-#SBATCH -p gpu-a100
+#SBATCH -p gh
 #SBATCH -t 48:00:00
-#SBATCH --nodes=4                # Number of nodes
-#SBATCH --ntasks-per-node=3
+#SBATCH --nodes=1                # Number of nodes
+#SBATCH --ntasks-per-node=1
 #SBATCH -o logs/large_train_adamw.o%j  # Output file
 #SBATCH -e logs/large_train_adamw.e%j   # Error file
 
 source ~/.bashrc
 conda deactivate
-cd /scratch/10152/baiyusu/litgpt
-conda activate llamaenv
+cd /scratch/10152/baiyusu/DiscreteFlow
+conda activate flowenv
 
 # Define MASTER_ADDR and MASTER_PORT
 MASTER_ADDR=$(srun --nodes=1 --ntasks=1 hostname | head -n1)
@@ -18,14 +18,12 @@ MASTER_PORT=12355
 
 export NCCL_IB_DISABLE=0
 NODE_RANK=$SLURM_NODEID
-GPUS_PER_NODE=3
+GPUS_PER_NODE=1
 
 # NCCL settings
 export NCCL_DEBUG=INFO
 
 export TORCHINDUCTOR_CACHE_DIR=/tmp/torchinductor_cache_${SLURM_PROCID}
 
-echo "Checking GPUs on each node..."
-srun -l bash -c 'hostname; nvidia-smi -L'
-
-torchrun --nproc_per_node=2 train.py --config configs/config.py
+# Not using distributed training for debug
+torchrun --nproc_per_node=1 train.py --config configs/config.py
